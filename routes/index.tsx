73099a38fa6c39aa1ea473af8b117eb3eb3b96b7
Junyoung <compilevoid@gmail.com>
2026-03-27 09:06:42 +0000

    update

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,30 +1,14 @@
-import { useSignal } from "@preact/signals";
-import { define } from "../utils.ts";
-import Counter from "../islands/Counter.tsx";
+import { define } from "@/utils.ts";
+import { Head } from "fresh/runtime";
 
-export default define.page(function Home(ctx) {
-  const count = useSignal(3);
-
-  ctx.state.title = count.value + " Fresh Counter" +
-    (Math.abs(count.value) === 1 ? "" : "s");
-
+export default define.page(() => {
   return (
-    <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
-    </div>
+    <main>
+      <Head>
+        <title>김준영</title>
+      </Head>
+      <h1>Junyong Kim 🇰🇷</h1>
+      <p>안녕하세요</p>
+    </main>
   );
 });
